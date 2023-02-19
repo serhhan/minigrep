21466a116c4f5cf541d54f305b69a11a6a4d8ea4
Serhan Elmalı <serhanelmali@gmail.com>
2023-02-20 02:13:29 +0300

    feat/read-a-file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -7,5 +8,10 @@ fn main() {
     let file_path = &args[2];
 
     println!("Searching for {}", query);
-    println!("In file {}", file_path)
+    println!("In file {}", file_path);
+    
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text\n{contents}");
 }
