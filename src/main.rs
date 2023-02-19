d59b77627594588751b84efe3e292f634dc60ce5
Serhan Elmalı <serhanelmali@gmail.com>
2023-02-20 02:34:07 +0300

    refactor/extract-argument-parser

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,8 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
-    let query = &args[1];
-    let file_path = &args[2];
+    
+    let (query, file_path) = parse_config(&args);
 
     println!("Searching for {}", query);
     println!("In file {}", file_path);
@@ -14,4 +13,11 @@ fn main() {
         .expect("Should have been able to read the file");
 
     println!("With text\n{contents}");
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+
+    (query, file_path)
 }
