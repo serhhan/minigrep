f91b8aad5ddb5a99390d6b72a42e1125de480bca
serhanelmali <serhanelmali@gmail.com>
2023-02-21 02:53:58 +0300

    enhancement/handling-errors-returned-from-run-in-main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,14 +14,18 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
     
-    run(config);
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    };
 }
 
-fn run(config: Config) {
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
     println!("With text:\n{contents}");
+
+    Ok(())
 }
 
 struct Config {
