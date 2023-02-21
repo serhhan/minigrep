f3085abfe8f6e2e7e03f1b8008c5ec1b09923725
serhanelmali <serhanelmali@gmail.com>
2023-02-22 00:34:33 +0300

    refactor/add-prefix-to-intentional-args

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = minigrep::Config::build(&args).unwrap_or_else(|err| {
+    let config = minigrep::Config::build(&args).unwrap_or_else(|_err| {
         process::exit(1);
     });
 
-    if let Err(e) = minigrep::run(config) {
+    if let Err(_e) = minigrep::run(config) {
         process::exit(1);
     };
 }
