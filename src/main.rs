1db19ab94ee8494d1394a3d38ef3b7e852035d58
serhanelmali <serhanelmali@gmail.com>
2023-02-23 02:43:41 +0300

    refactor/error-msg-std-output-to-std-error

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@ use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = minigrep::Config::build(&args).unwrap_or_else(|_err| {
+    let config = minigrep::Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    if let Err(_e) = minigrep::run(config) {
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
         process::exit(1);
     };
 }
